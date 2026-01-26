8a82bf1be54d6a5c865f9b6c74425bff8f2a1423
formeo <formeo@yandex.ru>
2026-01-26 19:49:54 +0300

    add telegram interations

--- voice_to_text/__init__.py
+++ voice_to_text/__init__.py
@@ -1,6 +1,6 @@
 """Voice-to-Text: Bulk voice message transcription using Whisper."""
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "Roman Gordienko"
 
 from voice_to_text.transcriber import (
@@ -8,6 +8,7 @@ from voice_to_text.transcriber import (
     ModelSize,
     TranscriptionResult,
     create_transcriber,
+    BaseTranscriber,
 )
 from voice_to_text.formats import (
     AudioFormat,
@@ -15,6 +16,9 @@ from voice_to_text.formats import (
     detect_format,
     get_audio_info,
     find_voice_files,
+    convert_to_wav,
+    is_supported,
+    get_supported_extensions,
 )
 
 __all__ = [
@@ -22,9 +26,24 @@ __all__ = [
     "ModelSize",
     "TranscriptionResult",
     "create_transcriber",
+    "BaseTranscriber",
     "AudioFormat",
     "AudioInfo",
     "detect_format",
     "get_audio_info",
     "find_voice_files",
+    "convert_to_wav",
+    "is_supported",
+    "get_supported_extensions",
 ]
+
+
+# Lazy import for integrations (avoid import errors if aiogram/ptb not installed)
+def __getattr__(name):
+    if name == "TelegramVoiceHandler":
+        from voice_to_text.integrations.telegram import TelegramVoiceHandler
+        return TelegramVoiceHandler
+    if name == "transcribe_telegram_voice":
+        from voice_to_text.integrations.telegram import transcribe_telegram_voice
+        return transcribe_telegram_voice
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
